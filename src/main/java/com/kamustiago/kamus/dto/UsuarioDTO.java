6210286bcdbb20b7fd90587e5768637e8710e946
KamusTiago <tiagokamus40@gmail.com>
2021-02-24 11:58:13 -0300

    usando padrao dto para retornar usuarios

---
+++ src/main/java/com/kamustiago/kamus/dto/UsuarioDTO.java
@@ -0,0 +1,50 @@
+package com.kamustiago.kamus.dto;
+
+import java.io.Serializable;
+
+import com.kamustiago.kamus.dominio.Usuario;
+
+//objetos convertidos em bytes para serem trafegados em rede, gravados em arquivos = Serializable
+public class UsuarioDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	private String id;
+	private String nome;
+	private String email;
+	
+	public UsuarioDTO() {
+		
+	}
+	
+	//Construtor com sobrecarga para instanciar a partir do objeto do tipo entidade usuario
+	public UsuarioDTO(Usuario objetoDoTipoUsuario) {	
+		id = objetoDoTipoUsuario.getId();
+		nome = objetoDoTipoUsuario.getNome();
+		email = objetoDoTipoUsuario.getEmail();		
+	}
+
+	//getters e setters
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
--- src/main/java/com/kamustiago/kamus/recursos/UsuarioRecurso.java
+++ src/main/java/com/kamustiago/kamus/recursos/UsuarioRecurso.java
@@ -1,6 +1,7 @@
 package com.kamustiago.kamus.recursos;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kamustiago.kamus.dominio.Usuario;
+import com.kamustiago.kamus.dto.UsuarioDTO;
 import com.kamustiago.kamus.servicos.UsuarioServico;
 
 @RestController // recurso rest
@@ -24,11 +26,18 @@ public class UsuarioRecurso {
 	//com posiveis cabeçalhos, erros, etc
 	//metodo ok() para instanciar responseentity ja com o codigo de resposta http informando "sucesso"
 	//body() vai definir o corpo da resposta a lista que eu montei
-	
 	@RequestMapping(method = RequestMethod.GET) 
-	public ResponseEntity<List<Usuario>> findAll() {
+	public ResponseEntity<List<UsuarioDTO>> findAll() {
 		List<Usuario> lista = servico.findAll();
-		return ResponseEntity.ok().body(lista);
+		
+		//convertendo uma lista de usuario para usuarioDTO com instrucao lambda
+		//metodo stream faz conversao para streamer para colecao compativel com expessoes lambda
+		//map() vai pegar cada objeto x da lista original e para cada obj que vai ser usuario, retorne new usuario passando o x
+		// e voltando o stream para uma lista uso o Collect( collectors.tolist())	
+		List<UsuarioDTO> listaDto = lista.stream().map(x -> new UsuarioDTO(x)).collect(Collectors.toList());
+		
+		//metodo retorna agor aum listaDTO
+		return ResponseEntity.ok().body(listaDto);
 	}
 
 }
